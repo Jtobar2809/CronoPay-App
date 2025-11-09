c63bb61432237d6149dc11ad8d564aba1a31c6bb
JhonGalindez <jhongalindez3@gmail.com>
2025-11-08 22:23:13 -0500

    Welcome Page

---
+++ src/app/(onboarding)/protected/index.tsx
@@ -0,0 +1,32 @@
+import { Stack } from "expo-router"
+import { Text, View } from "react-native"
+import { SafeAreaView } from "react-native-safe-area-context"
+
+import { useAuth } from "../../../../providers/AuthProvider"
+
+export default function Page() {
+  const { loading, session } = useAuth()
+
+  if (loading) {
+    return (
+      <SafeAreaView className="flex-1 items-center justify-center">
+        <Text>Cargando...</Text>
+      </SafeAreaView>
+    )
+  }
+
+  return (
+    <>
+      <Stack.Screen options={{ headerShown: false }} />
+      <SafeAreaView className="flex-1 bg-[#faedcd]">
+        <View className="flex-1 items-center justify-center">
+          <Text className="font-medium text-[#838383]">Welcome,</Text>
+          <Text className="text-lg font-bold text-[#7f4f24]">
+            {session?.user?.user_metadata?.full_name ??
+              session?.user?.email?.split("@")[0]}
+          </Text>
+        </View>
+      </SafeAreaView>
+    </>
+  )
+}
